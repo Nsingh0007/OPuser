11872f71d35ec272a25d3e6ba39f3942c4bc508c
Anjali459 <adubey@moreyeahs.in>
2023-03-09 10:22:35 +0530

    updated code for structure

--- src/Pages/auth/uploadPhoto/index.js
+++ src/Pages/auth/uploadPhoto/index.js
@@ -1,7 +1,15 @@
-import React from 'react'
-import { Upload } from './upload'
+import React from "react";
+import { Upload } from "./upload";
+import img from "../../../assets/images/Group 33630.png";
 export default function UploadPage() {
-    return (
-        <Upload />
-    )
+  return (
+    <div className="outerflex">
+      <div className="left-flex ">
+        <div className="container-flex">
+          <Upload />
+        </div>
+      </div>
+      <img src={img} alt="" className="right-flex"/>
+    </div>
+  );
 }
