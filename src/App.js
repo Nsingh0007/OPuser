c31aef47bef309a93cfd9892301ce0a25a27e28a
Anjali459 <adubey@moreyeahs.in>
2023-03-13 18:55:35 +0530

    auth

--- src/App.js
+++ src/App.js
@@ -1,40 +1,51 @@
 import { toJS } from "mobx";
 import { observer } from "mobx-react-lite";
 import { useLayoutEffect } from "react";
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
-import './App.css';
-import BaseLayout from './common/baseLayout';
+import "./App.css";
+import BaseLayout from "./common/baseLayout";
 import Loader from "./customComponents/loader/loader";
-import AuthStore from './mobx/auth';
-import Otp from './Pages/auth/password/otp';
+import AuthStore from "./mobx/auth";
+import Otp from "./Pages/auth/password/otp";
 import PrivateLayout from "./Pages/PublicPages/PrivateRoute";
-import { RouteConstant } from './utils/routes/constant';
+import CheckAuth from "./utils/hooks/checkAuth";
+import { RouteConstant } from "./utils/routes/constant";
 
 function App() {
   const auth = toJS(AuthStore?.user?.user);
   const loading = toJS(AuthStore?.isLoading);
   const loginData = localStorage.getItem("key");
-  // const [toggle, setToggle] = useState(false)
+  const isVerifiedUser = CheckAuth()
 
   useLayoutEffect(() => {
-    // console.log("loginData", loginData)
+    // console.log("----auth", toJS(AuthStore?.user?.token));
     AuthStore.setUser(JSON.parse(loginData));
     // setToggle(true)
   }, [loginData]);
 
-  // if (!toggle) return <div>Loading...</div>
+  
+
+ 
   return (
     <div>
       <ToastContainer />
       {loading && <Loader />}
       {auth && (
-        <Routes>  
-          {/* <Route path='/' element={<PrivateLayout />} > */}
-          <Route path="/" element={<Navigate to="/verification" />} />
-          <Route path='verification' element={<Otp />} />
-          <Route path='*' element={<Navigate to="/verification" />} />
+        <Routes>
+          {isVerifiedUser? (
+            <>
+
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<Navigate to="/verification" />} />
+              <Route path="verification" element={<Otp />} />
+              <Route path="*" element={<Navigate to="/verification" />} />
+            </>
+          )}
+
           {/* </Route> */}
         </Routes>
       )}
@@ -51,6 +62,6 @@ function App() {
         </Route>
       </Routes> */}
     </div>
-  )
+  );
 }
 export default observer(App);
---
+++ src/utils/hooks/checkAuth.js
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+import AuthStore from "../../mobx/auth";
+const CheckAuth=()=>{
+   if(AuthStore?.user?.token  &&  AuthStore?.user?.user?.isVerified  &&  AuthStore?.user?.user?.institute  && AuthStore?.user?.user?.course ){
+    return true
+   }
+}
+export default CheckAuth
